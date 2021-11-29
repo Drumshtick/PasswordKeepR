b7be06e5c1e859130d6cb0318795698a0866aa4a
Drumshtick <ekodnar@outlook.com>
2021-11-29 21:55:54 +0000

    register logic and changes to page

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,19 @@
 
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const { ifUserEmailExists } = require('./helpers');
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    res.render("register");
+  router.get('/', (req, res) => {
+    res.render('register');
   });
+
+  router.post('', (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.redirect(400, '/register');
+    } else if (ifUserEmailExists(email))
+  });
+
   return router;
 };
