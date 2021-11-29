6d19a91308041144bde0650901919327d8035796
Drumshtick <ekodnar@outlook.com>
2021-11-29 17:13:55 +0000

    created listener post /new_org

--- routes/create_org.js
+++ routes/create_org.js
@@ -1,1 +1,24 @@
-// features/routes/create_organization
+//branch features/routes/create_organization
+/*
+Create a new organization
+*/
+const express = require('express');
+const router = express.Router();
+const { db, Pool } = require('../db/dbConn');
+
+module.exports = (db) => {
+  router.post('/new_org', (req, res) => {
+    const { user_id } = req.session;
+    // Check for session cookie created by login
+    if (!user_id) {
+      res.redirect('/login');
+    } else if (user_id) {
+
+    }
+
+  });
+};
+
+const addOrg = (user, org_name) => {
+
+};
