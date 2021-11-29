392378873eb2bc7e5995d566f4db990733eba4a2
Drumshtick <ekodnar@outlook.com>
2021-11-29 17:39:45 +0000

    finished post /new_org

--- routes/create_org.js
+++ routes/create_org.js
@@ -8,6 +8,7 @@ const { Pool } = require('../db/dbConn');
 
 module.exports = (db) => {
   router.post('/new_org', (req, res) => {
+    // Assumes user_id session cookie = user id from TABLE users
     const { user_id } = req.session;
     const { org_name } = req.body;
     // Check for session cookie created by login
@@ -15,14 +16,34 @@ module.exports = (db) => {
       res.redirect('/login');
     } else if (user_id) {
       addOrg(user_id)
+      .then(organization => {
+        if (!organization) {
+          console.log("---------ERROR IN Creating entry in organization (improper return)--------");
+          return;
+        } else if (organization) {
+          res.redirect('index');
+        }
+      })
+      .catch((error) => {
+        console.log("---------ERROR IN promise from addOrg--------");
+        console.log(error.message);
+      });
     }
-
   });
 };
 
 const addOrg = (user_id, org_name) => {
+  // assumes user_id is the id from the users TABLE
   return Pool
-  .query(``).then()
+  .query(`
+  INSERT INTO
+  organizations (name)
+  VALUES ($1)
+  RETURNING *;
+  `, [org_name])
+  .then((result) => {
+    return result.rows[0];
+  })
   .catch((error) => {
     console.log("---------ERROR IN addORG query--------");
     console.log(error.message);
