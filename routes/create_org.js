d7605d1909cdff830d8da16fc6bb1ca6adc6746c
Drumshtick <ekodnar@outlook.com>
2021-11-29 17:21:45 +0000

    created addOrg helper func :ahhhhhhhhh:

--- routes/create_org.js
+++ routes/create_org.js
@@ -4,21 +4,27 @@ Create a new organization
 */
 const express = require('express');
 const router = express.Router();
-const { db, Pool } = require('../db/dbConn');
+const { Pool } = require('../db/dbConn');
 
 module.exports = (db) => {
   router.post('/new_org', (req, res) => {
     const { user_id } = req.session;
+    const { org_name } = req.body;
     // Check for session cookie created by login
     if (!user_id) {
       res.redirect('/login');
     } else if (user_id) {
-
+      addOrg(user_id)
     }
 
   });
 };
 
-const addOrg = (user, org_name) => {
-
+const addOrg = (user_id, org_name) => {
+  return Pool
+  .query(``).then()
+  .catch((error) => {
+    console.log("---------ERROR IN addORG query--------");
+    console.log(error.message);
+  });
 };
