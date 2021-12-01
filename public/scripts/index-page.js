c35255ae7fe5764146e680ecce336e87951f4b6b
Drumshtick <ekodnar@outlook.com>
2021-12-01 22:06:11 +0000

    cleaned up code

--- public/scripts/index-page.js
+++ public/scripts/index-page.js
@@ -17,39 +17,44 @@ $(document).ready(function() {
       });
   });
 
-$('button.edit').on('click touchstart', function() {
+  $('button.edit').on('click touchstart', function() {
+    // When edit button is clicked
+    // create input fields and changed edit to save button
+    const targetParent =$(this).parent().parent();
 
-  const targetParent =$(this).parent().parent();
-  const username =$(targetParent).children('section.password-data')
-  .children('div.data-wrapper.username').children('div.value-wrapper')
-  .children('p.data-value');
-
-  const password =$(targetParent).children('section.password-data')
-  .children('div.data-wrapper.password').children('div.value-wrapper')
-  .children('p.data-value');
-
-  const button = $(this);
-  $(username).replaceWith('<input class="data" type="text" name="username" class="form-control" value="'+$(username).text()+'">');
-  $(password).replaceWith('<input class="data" type="text" name="password-text" class="form-control" value="'+$(password).text()+'">');
-  $(button).replaceWith('<button type="button" style="background-color: purple;" class="edit save btn btn-primary">Save</button>');
-  $('button.save').on('click', function() {
-  const targetParent =$(this).parent().parent();
-  const username =$(targetParent).children('section.password-data')
+    const username =$(targetParent).children('section.password-data')
     .children('div.data-wrapper.username').children('div.value-wrapper')
-    .children('input.data').val();
+    .children('p.data-value');
 
-  const password =$(targetParent).children('section.password-data')
+    const password =$(targetParent).children('section.password-data')
     .children('div.data-wrapper.password').children('div.value-wrapper')
-    .children('input.data').val();
-  const url = $(targetParent).children('header').children('div').children('a').text();
+    .children('p.data-value');
 
-  console.log("values: ", username, password);
-$.post('http://localhost:8080/edit', {username, password, url})
-  .then((response) => {console.log(response)})
-  .catch((err) => {console.log(err.message)})
-});
-});
+    const button = $(this);
 
+    $(username).replaceWith('<input class="data" type="text" name="username" class="form-control" value="'+$(username).text()+'">');
+
+    $(password).replaceWith('<input class="data" type="text" name="password-text" class="form-control" value="'+$(password).text()+'">');
+
+    $(button).replaceWith('<button type="button" style="background-color: purple;" class="edit save btn btn-primary">Save</button>');
+    // Event listener for save button to make the POST
+    $('button.save').on('click', function() {
+
+      const targetParent =$(this).parent().parent();
+
+      const username =$(targetParent).children('section.password-data')
+        .children('div.data-wrapper.username').children('div.value-wrapper')
+        .children('input.data').val();
+
+      const password =$(targetParent).children('section.password-data')
+        .children('div.data-wrapper.password').children('div.value-wrapper')
+        .children('input.data').val();
+      const url = $(targetParent).children('header').children('div').children('a').text();
+
+      $.post('http://localhost:8080/edit', {username, password, url})
+        .catch((err) => {console.log(err.message)});
+    });
+  });
 
 });
 
--- routes/edit.js
+++ routes/edit.js
@@ -16,17 +16,19 @@ editRouter.post('/', (req, res) => {
 
   const { user_id } = req.session;
   const { username, password, url} = req.body;
+  // To reply to ajax stating success
+  res.sendStatus(200);
 
-  res.send(200);
-const data = {
-username,
-user_id,
-password,
-url
-};
+  const data = {
+    username,
+    user_id,
+    password,
+    url
+  };
+
   editPasswordEntry(data)
   .then((db_results) => {
-console.log(db_results);
+console.log("UPDATED Table");
   })
 });
 
@@ -37,31 +39,34 @@ module.exports = editRouter;
 const editPasswordEntry = function(data){
   let query = `
               UPDATE passwords
-              SET
+              SET password_text = $1,
+              username = $2
+              WHERE user_id = $3
+              AND url = $4 RETURNING *;
               `;
-  const queryParams = [];
+  const queryParams = [data.password, data.username, data.user_id, data.url];
 
 
-  if (data.password_text !== undefined) {
-    queryParams.push(data.password);
-    query += ` password_text = $${queryParams.length} `
-  }
+  // if (data.password !== undefined) {
+  //   queryParams.push(data.password);
+  //   query += ` password_text = $${queryParams.length}, `
+  // }
 
-  if (data.username !== undefined) {
-    queryParams.push(data.username);
-    query += ` username = $${queryParams.length} `
-  }
 
-  queryParams.push(data.user_id);
-  query += ` WHERE user_id = $${queryParams.length} `;
-  queryParams.push(data.url);
-  query += ` AND url = $${queryParams.length} RETURNING *;`
-  console.log("query string: ", query);
-  console.log("query params: ", queryParams);
+  //   queryParams.push(data.username);
+  //   query += ` username = $${queryParams.length} `
+  // }
+
+  // queryParams.push(data.user_id);
+  // query += ` WHERE user_id = $${queryParams.length} `;
+
+  // queryParams.push(data.url);
+  // query += ` AND url = $${queryParams.length} RETURNING *;`
+
+  console.log(query);
   return db
   .query(query, queryParams)
   .then((db_results) => {
-    console.log(db_results);
     return db_results.rows;
   })
   .catch((err) => {
